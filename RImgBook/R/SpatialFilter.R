e931e2973632765faf1f159886d913f3cf8cc74f
takeo.katsuki <takeo.katsuki@gmail.com>
2011-10-30 05:09:33 +0000

    fixed indent

--- RImgBook/R/SpatialFilter.R
+++ RImgBook/R/SpatialFilter.R
@@ -3,10 +3,10 @@ SpatialFilter <- function(img, weight) {
 	wc <- ncol(weight)
 	row <- nrow(img)
 	col <- ncol(img)
-	a <- matrix(0, row, col) # prepare matrix
+	a <- matrix(0, row, col)
 	for(i in wr:(row-wr+1)) for(j in wc:(col-wc+1)) {
-	      a[i,j] <- crossprod(as.vector(img[(i-wr+1):i,(j-wc+1):j]),
-                                  as.vector(weight))
-	}
+    a[i,j] <- crossprod(as.vector(img[(i-wr+1):i,(j-wc+1):j]),
+                        as.vector(weight))
+  }
 	a
 }
--- RImgBook/R/SpatialFilter2.R
+++ RImgBook/R/SpatialFilter2.R
@@ -1,21 +1,21 @@
 SpatialFilter2 <- function(img, weight, r=1) {
-row <- nrow(img)
-col <- ncol(img)
-w <- as.vector(weight)
-# padding
-img <- rbind(matrix(0, r, col+2*r), 
-                 cbind(matrix(0, row, r), img, matrix(0, row, r)), 
-                 matrix(0, r, col+2*r))
-# stacking
-stack <- array(c(w[1]*img[1:row, 1:col],
-                 w[2]*img[1:row, 2:(col+1)], 
-                 w[3]*img[1:row, 3:(col+2)],
-                 w[4]*img[2:(row+1), 1:col], 
-                 w[5]*img[2:(row+1), 2:(col+1)],
-                 w[6]*img[2:(row+1), 3:(col+2)],  
-                 w[7]*img[3:(row+2), 1:col],
-                 w[8]*img[3:(row+2), 2:(col+1)],
-                 w[9]*img[3:(row+2), 3:(col+2)]), 
+  row <- nrow(img)
+  col <- ncol(img)
+  w <- as.vector(weight)
+  # padding
+  img <- rbind(matrix(0, r, col+2*r), 
+               cbind(matrix(0, row, r), img, matrix(0, row, r)), 
+               matrix(0, r, col+2*r))
+  # stacking
+  stack <- array(c(w[1]*img[1:row, 1:col],
+                   w[2]*img[1:row, 2:(col+1)], 
+                   w[3]*img[1:row, 3:(col+2)],
+                   w[4]*img[2:(row+1), 1:col], 
+                   w[5]*img[2:(row+1), 2:(col+1)],
+                   w[6]*img[2:(row+1), 3:(col+2)],  
+                   w[7]*img[3:(row+2), 1:col],
+                   w[8]*img[3:(row+2), 2:(col+1)],
+                   w[9]*img[3:(row+2), 3:(col+2)]), 
                  c(row, col, 9))
-a <- rowSums(stack, dim=2)
+  a <- rowSums(stack, dim=2)
 }
