de8e6858cc4bcfb052818a940d22c45376f6d1e2
takeo.katsuki <takeo.katsuki@gmail.com>
2011-11-02 02:53:14 +0000

    removed unused demos

--- RImgBook/demo/OpticalFlowDemo.R
+++ RImgBook/demo/OpticalFlowDemo.R
@@ -1,21 +1,21 @@
-require(RImageBook)
-img1 <- readImage(system.file("images/frame159.jpeg",package="RImageBook"))
-img2 <- readImage(system.file("images/frame160.jpeg",package="RImageBook"))
-ft1 <- GoodFeaturesToTrack(img1, 200) # 特徴点の自動検出
-res <- OpticalFlow(img1, img2, ft1)   # オプティカルフローの計算
-ft1 <- ft1[1:2,res[3,]>0]             # 検出成功した点以外をフィルタリング
-res <- res[1:2,res[3,]>0]
-# 以下，結果の作図
-oldpar <- par(no.readonly = TRUE)
-png(filename = "temp.png", width = nrow(img1), 
-    height = ncol(img1), bg = "black")
+pen <- readImage(system.file("images/pendulum.gif", package="RImageBook"))
+img0 <- pen[,,1]
+img1 <- pen[,,4]
+ftr <- GoodFeaturesToTrack(img0, 200)
+res <- OpticalFlow(img0, img1, ftr, 40)
+ftr <- ftr[1:2, res[3,]>0]             # 検出成功した点以外をフィルタリング
+res <- res[1:2, res[3,]>0]
+w <- nrow(img0)
+h <- ncol(img0)
+png(filename = "temp.png", width = w, height = h, bg = "black")
 par(plt=c(0, 1, 0, 1), xaxs="i", yaxs="i")
-plot(t(ft1)[,1], ncol(img1)-t(ft1)[,2], axes=F, pch=20, 
-     col="white", xlim=c(0, nrow(img1)), ylim=c(0, ncol(img1)))
-arrows(t(ft1)[,1], ncol(img1)-t(ft1)[,2], t(res)[,1], ncol(img1)-t(res)[,2], 
+plot(t(ftr)[,1], h - t(ftr)[,2], axes=F, pch=20, 
+     col="white", xlim=c(0, w), ylim=c(0, h))
+arrows(t(ftr)[,1], h-t(ftr)[,2], t(res)[,1], h-t(res)[,2], 
        col="white", length=0.02)
 dev.off()
 img <- readImage("temp.png")
-display(img1 + img)
+display(img)
+img0[img0 == 0] <- 0.5
+display((1-img) * img0)
 unlink("temp.png")
-par(oldpar)
