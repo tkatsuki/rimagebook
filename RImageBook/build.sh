95a9e5846cf59940d6b8aa620cb2c720733fd93a
yhourai <yhourai@gmail.com>
2011-11-23 17:48:09 +0000

    revised build.sh

--- RImageBook/build.sh
+++ RImageBook/build.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
+
+BUILD_TARGET=RImageBook_1.0.tar.gz
 case $OS in
 Windows*)
   echo This script is for linux OS.
   ;;
 *)
-  echo "src/thirdparty/.*" > .Rbuildignore
-  mv NAMESPACE NAMESPACE.bak || exit 1
-  cp NAMESPACE.linux NAMESPACE || exit 1
-  pushd ..
-  if [ ! -e RImageBook_1.0.tar.gz ]; then
-    R CMD build RImageBook || exit 1
+  if [ ! -e ../${BUILD_TARGET} ]; then
+    echo "src/thirdparty/.*" > .Rbuildignore
+    mv NAMESPACE NAMESPACE.bak || exit 1
+    cp NAMESPACE.linux NAMESPACE || exit 1
+    pushd ..
+    echo Building ${BUILD_TARGET}
+    R CMD build RImageBook
+    popd
+    mv NAMESPACE.bak NAMESPACE
+    rm -f .Rbuildignore
+  else
+    echo ${BUILD_TARGET} already exists. Skipping build
   fi
-  R CMD INSTALL RImageBook_1.0.tar.gz
-  popd 
-  mv NAMESPACE.bak NAMESPACE
-  rm -f .Rbuildignore
+  if [ -e ../${BUILD_TARGET} ]; then
+    echo Installing
+    R CMD INSTALL ../${BUILD_TARGET}
+  else
+    echo build failed
+    exit 1
+  fi
 esac
 
