6164569dffff25559946325e985d6b6c88f47839
takeo.katsuki <takeo.katsuki@gmail.com>
2012-02-04 04:43:13 +0000

    VRDemo renderer type changed.

--- RImageBook/demo/VRDemo.R
+++ RImageBook/demo/VRDemo.R
@@ -16,4 +16,4 @@ brain <- f.read.analyze.volume(system.file("example.img",
 brain <- brain[,,,1]
 VolumeRendering(brain, c(0, 3000, 8000, 10000), 
                 c(0,0,0,0, 0.2,1,1,1, 0.5,1,0,0, 1,0,1,0), 
-                Spacing=c(1.0,1.0,1.5), RendererType=1)
+                Spacing=c(1.0,1.0,1.5), RendererType=0)
