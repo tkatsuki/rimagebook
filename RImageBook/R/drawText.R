e8df989410eab4e8fcb002c03160917ef1835990
takeo.katsuki <takeo.katsuki@gmail.com>
2013-09-19 18:25:46 +0000

    drawText() is still buggy but works OK.

--- RImageBook/R/drawText.R
+++ RImageBook/R/drawText.R
@@ -1,10 +1,7 @@
 drawText <- function(img, text, x=ncol(img)/2, y=10, ...){
+  # Somehow the image gets shifted and leaves black line at the right and bottom
   require(RImageBook)
-  if(length(dim(img))!=2) stop("Only 2D array is supported.")
-  img <- cbind(rep(0, nrow(img)), img) 
-  img <- rbind(rep(0, ncol(img)), img)
   imgb <- E2b(img)
-  .pardefault <- par(no.readonly = T)
   png(filename = "temp.png", width = dim(img)[1], height = dim(img)[2], bg = "black")
   par(mar=c(0, 0, 0, 0))
   plot(imgb)
@@ -12,6 +9,5 @@ drawText <- function(img, text, x=ncol(img)/2, y=10, ...){
   dev.off()
   img <- readImage("temp.png")
   unlink("temp.png")
-  par(.pardefault)
   img
 }
